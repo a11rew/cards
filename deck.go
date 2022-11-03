d3a4506284c37eac5a794279e05d517272f14c7d
Andrew Glago <andrewglago1@gmail.com>
2022-11-03 23:38:30 +0000

    feat: add write, read, shuffle

--- deck.go
+++ deck.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 type deck []string
 
@@ -27,4 +33,45 @@ func (d deck) print() {
 
 func deal(d deck, handsize int) (deck, deck) {
 	return d[:handsize], d[handsize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join(d, ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	stringrep := d.toString()
+
+	error := os.WriteFile(filename, []byte(stringrep), 0666)
+	return error
+}
+
+func newDeckFromFile(filename string) deck {
+	// Read file into byte slice
+	bytesrep, err := os.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error reading file: ", err)
+		os.Exit(1)
+	}
+
+	// Convert byte slice to string
+	stringrep := string(bytesrep)
+
+	// Split into slice
+	cards := strings.Split(stringrep, ",")
+
+	return deck(cards)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		np := r.Intn(len(d) - 1)
+
+		// Swap oneliner
+		d[i], d[np] = d[np], d[i]
+	}
 }
