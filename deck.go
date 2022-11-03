3a207ddd4a82834075286dc28b4abdfb3c3082a0
Andrew Glago <andrewglago1@gmail.com>
2022-11-03 22:22:09 +0000

    feat: add deal support

--- deck.go
+++ deck.go
@@ -8,7 +8,7 @@ func newDeck() deck {
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
-	cardValues := []string{"Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One" }
+	cardValues := []string{"Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
@@ -23,4 +23,8 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handsize int) (deck, deck) {
+	return d[:handsize], d[handsize:]
 }
